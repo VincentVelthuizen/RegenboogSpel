e215919867a33f8f3e67283f98c647f163b03116
Vincent <vincent.velthuizen@gmail.com>
2026-04-18 18:01:58 +0200

    feat(game): voeg kleur- en emoji-constanten toe

---
+++ js/game/colors.js
@@ -0,0 +1,19 @@
+export const COLORS = ['purple', 'blue', 'green', 'yellow', 'orange', 'red'];
+
+export const COLOR_HEX = {
+  purple: '#9b59b6',
+  blue:   '#3498db',
+  green:  '#2ecc71',
+  yellow: '#f1c40f',
+  orange: '#e67e22',
+  red:    '#e74c3c',
+};
+
+export const EMOJI_POOL = {
+  purple: ['🍆', '🍇', '🟣', '🟪', '👾', '🔮'],
+  blue:   ['🫐', '🐳', '🟦', '🔵', '💙', '🌀'],
+  green:  ['🐸', '🥒', '🟢', '🟩', '🥬', '🌲'],
+  yellow: ['🍌', '🌻', '🟡', '🟨', '⭐', '🧀'],
+  orange: ['🍊', '🥕', '🟠', '🟧', '🦊', '🎃'],
+  red:    ['🍎', '🍓', '🔴', '🟥', '❤️', '🚒'],
+};
